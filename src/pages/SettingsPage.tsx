24e38a5d55ae23a25ac5ede286a9f9c3d386858c
Benjamin <benferanmiopafunso@gmail.com>
2026-06-23 02:14:30 +0100

    new fixes and updates

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -256,7 +256,7 @@ export function SettingsPage() {
           </CardContent>
         </Card>
 
-        {/* <Card>
+        <Card>
           <CardHeader className="flex flex-row items-center justify-between">
             <CardTitle className="text-base">Social Links</CardTitle>
             <Button
@@ -301,8 +301,8 @@ export function SettingsPage() {
               </div>
             ))}
           </CardContent>
-        </Card> */}
-        {/* 
+        </Card>
+
         <Card>
           <CardHeader>
             <CardTitle className="text-base">Business Hours</CardTitle>
@@ -365,7 +365,7 @@ export function SettingsPage() {
               />
             </div>
           </CardContent>
-        </Card> */}
+        </Card>
 
         <Button type="submit" disabled={update.isPending}>
           {update.isPending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
